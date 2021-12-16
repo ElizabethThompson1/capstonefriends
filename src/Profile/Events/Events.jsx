ba0b167fd7b87c162bac004d8af722052fc4575a
ElizabethThompson1 <usawsoccer11@yahoo.com>
2021-12-16 11:27:19 -0600

    added multiple folders as a layout. also pulled in the api for events

---
+++ src/Profile/Events/Events.jsx
@@ -0,0 +1,13 @@
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+
+
+const EventDisplay = (props) => {
+    let urlLink = `https://www.youtube.com/embed/${props.videoId}?autoplay=1&origin=http://example.com`
+    return(
+    <iframe className= "video" id="ytplayer" type="text/html" width="640" height="360"
+    src= {urlLink}
+    frameBorder="0">
+    </iframe>
+    )
+}
---
+++ src/Profile/Profile.jsx
@@ -0,0 +1,37 @@
+import React = require("react");
+
+function Profile (){
+    const [event,setEvent] = useState(null);
+    const [searchTerm,setSearchTerm] =useState("swimming")
+    const [eventId,setEventId] = useState("")
+
+    useEffect(() => {
+        getEvents();
+    }, [])
+    
+    //need to chnage the url to the events url
+   async function getEvents(){
+    const response = await axios.get (`https://www.googleapis.com/youtube/v3/search?key=AIzaSyBYDM03FFHGen7v_d4NCi2TOLMDtWLhPrk&maxResults=30&q=${searchTerm}&type=video`);
+    console.log(response.data);
+    setEvent(response.data.items);
+    setEventId(response.data.items[0].id.videoId)
+    } 
+
+    return(
+        <div className = "app">
+            {youTubeData &&
+                <div className ="app">
+                    <SearchBar value = {setSearchTerm}/>
+                    <VideoDisplay eventId = {eventId} />
+                    {/* <CommentDisplay commentId= {commentId} postComment={postComment}/> */}
+                        <ul>
+                            {comments.map((comment)=> (comment.videoId===videoId)?<li>{comment.text}<ul> {comment.replies.map((reply)=> <li>{reply.text}</li>)}</ul></li>:null)}
+                        </ul>
+                    {/* <OtherVideos videos = {searchTerm}/> */}
+                    <Footer/>
+           
+                </div>
+            }
+        </div>
+    );
+}
